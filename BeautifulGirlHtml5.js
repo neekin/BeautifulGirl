cbe558dafc2669f607cb1ca0e65b1e053013a9b3
NeeKin <njgzs360@gmail.com>
2018-08-06 14:10:28 +0800

    add test

---
+++ BeautifulGirlHtml5.js
@@ -0,0 +1,11 @@
+(function(window){
+window.onload = function(){
+           var canvas = document.createElement('canvas');
+           canvas.width = 600;
+           canvas.height= 600;
+           canvas.style='border:1px solid #0094ff;'
+           document.body.appendChild(canvas);
+
+}
+
+})(window)
